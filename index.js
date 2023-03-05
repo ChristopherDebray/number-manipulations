b4ba39106f27ac07833db75fa7c05180d1f72621
Christopher Debray <christopher.debray@club-employes.com>
2023-03-05 23:06:11 +0100

    ADD basic number operations

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+
+function subtract(numberOne, numberTwo) {
+    return numberOne - numberTwo;
+}
+
+function multiply(numberOne, numberTwo) {
+    return numberOne * numberTwo;
+}
+
+function divide(numberOne, numberTwo) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero.");
+    }
+    return numberOne / numberTwo;
+}
+
